146ef053ef51625d519ab927b57de07ce0768ea9
Arkadiy <greslayer@gmail.com>
2020-12-12 13:54:51 +0300

    Class loader task

---
+++ src/main/java/tasks/classloader/CustomClassLoader.java
@@ -0,0 +1,34 @@
+package tasks.classloader;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class CustomClassLoader extends ClassLoader{
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        byte[] b = loadClassFromFile(name);
+        return defineClass(name,b,0,b.length);
+    }
+
+    private byte[] loadClassFromFile(String name) {
+        InputStream inputStream = getClass().getClassLoader()
+                .getResourceAsStream(name.replace('.', File.separatorChar)+".class");
+        byte[] buffer;
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        int nextValue=0;
+        try {
+            assert inputStream != null;
+            while ((nextValue=inputStream.read())!=-1){
+                stream.write(nextValue);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        buffer=stream.toByteArray();
+        return buffer;
+    }
+}
+
